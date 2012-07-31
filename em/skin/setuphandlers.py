add9fdd80028aa2d3ffbc16116d6e658fe6d1694
Mike Muzurakis <clopy@unweb.me>
2012-07-31 17:52:19 +0300

    Added projects folder and moved it to the footer

---
+++ em/skin/setuphandlers.py
@@ -0,0 +1,19 @@
+import logging
+from Products.CMFCore.utils import getToolByName
+
+def setupVarious(context):
+    # Ordinarily, GenericSetup handlers check for the existence of XML files.
+    # Here, we are not parsing an XML file, but we use this text file as a
+    # flag to check that we actually meant for this import step to be run.
+    # The file is found in profiles/default.
+
+    portal = context.getSite()
+    logger = logging.getLogger('em.skin')
+
+    #create and publish the Projects folder if it is not already there
+    wftool = getToolByName(portal,'portal_workflow')
+    try:    
+        projects = portal.invokeFactory("Folder", "Projects")
+        wftool.doActionFor(portal[projects],action='publish')
+    except:
+        pass
