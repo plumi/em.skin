a4a4fe87930164210aa1489ab2eb4b46baad71b9
Mike Muzurakis <clopy@unweb.me>
2012-08-02 14:18:32 +0300

    override error, login and logout portlets to display header/footer

--- em/skin/browser/interfaces.py
+++ em/skin/browser/interfaces.py
@@ -1,4 +1,9 @@
 from zope.interface import Interface
+from plonetheme.classic.browser.interfaces import IThemeSpecific as IClassicTheme
+
+
+class IThemeSpecific(IClassicTheme):
+    """theme-specific layer"""
 
 
 class IEmSkinLayer(Interface):
