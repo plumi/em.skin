1f549aab0912c6cdc3537182879589f6d3b575ed
Dimitris Moraitis <dimo@unweb.me>
2012-08-04 00:51:51 +1000

    fix calwidget & display Collections in project slot

--- em/skin/browser/portlets/projects.py
+++ em/skin/browser/portlets/projects.py
@@ -53,7 +53,7 @@ class Renderer(base.Renderer):
         folder = portal['Projects']
         folder_path = '/'.join(folder.getPhysicalPath())
         portal_catalog = getToolByName(context, 'portal_catalog')
-        results = portal_catalog(portal_type='Document', path={'query': folder_path, 'depth': 1}, sort_on="getObjPositionInParent")
+        results = portal_catalog(portal_type=['Document','Topic'], path={'query': folder_path, 'depth': 1}, sort_on="getObjPositionInParent")
         return self.request.get(
             'items', results)
 
