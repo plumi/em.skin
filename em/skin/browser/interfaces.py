7e0831b5bce642541beda5f6f75a0f33333d9698
Mike Muzurakis <clopy@unweb.me>
2013-07-19 15:59:40 +0700

    uncommited stuff pending on em.skin

--- em/skin/browser/interfaces.py
+++ em/skin/browser/interfaces.py
@@ -1,5 +1,9 @@
 from zope.interface import Interface
 from plone.theme.interfaces import IDefaultPloneLayer
+from collective.captcha.form.field import Captcha
+
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory("plumi")
 
 class IThemeSpecific(IDefaultPloneLayer):
     """theme-specific layer"""
@@ -7,3 +11,16 @@ class IThemeSpecific(IDefaultPloneLayer):
 
 class IEmSkinLayer(Interface):
     """Marker interface for browserlayer."""
+
+class IEMRegistrationForm(Interface):
+    """Marker interface for my custom registration form
+    """
+
+
+class ICaptchaSchema(Interface):
+    captcha = Captcha(
+        title=_(u'Verification'),
+        description=_(
+            u'Type the code from the picture shown below.'
+        ),
+    )
