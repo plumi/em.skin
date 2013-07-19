55abb3fcd04b63b9c01fa166fa5a1c8fa9871a67
Mike Muzurakis <clopy@unweb.me>
2013-07-19 12:20:33 +0000

    remove recaptcha stuff

--- em/skin/browser/interfaces.py
+++ em/skin/browser/interfaces.py
@@ -1,10 +1,6 @@
 from zope import schema
 from zope.interface import Interface
 from plone.theme.interfaces import IDefaultPloneLayer
-from collective.captcha.form.field import Captcha
-
-from zope.i18nmessageid import MessageFactory
-_ = MessageFactory("plumi")
 
 class IThemeSpecific(IDefaultPloneLayer):
     """theme-specific layer"""
@@ -34,16 +30,3 @@ class IEmSettings(Interface):
                              default = u"",
                              required=False,
                             )
-
-class IEMRegistrationForm(Interface):
-    """Marker interface for my custom registration form
-    """
-
-
-class ICaptchaSchema(Interface):
-    captcha = Captcha(
-        title=_(u'Verification'),
-        description=_(
-            u'Type the code from the picture shown below.'
-        ),
-    )
